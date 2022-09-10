c443150f4454f339be340f57f74ea9c7437ea27e
grishanych <alsolo2011@gmail.com>
2022-09-10 07:58:30 +0300

    Add editing name, updating time and some styles

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,31 +1,45 @@
 import React, { useState } from "react";
+import { Row, Col, Button, FormControl  } from "react-bootstrap";
 import { v4 as uuidv4 } from 'uuid';
 
 function AddTodo({ todo, setTodo }) {
 
-    const [value, setValue] = useState('')
+    const [value, setValue] = useState('');
+    const [name, setName] = useState('');
+    const [showDate, setDate] = useState(new Date().toLocaleString()); 
+    
+
+    const handelTime = () =>{
+        let time = new Date().toLocaleString(); 
+        setDate(time);
+      }
+
 
     function saveTodo() {
-        if(value) {
+        if(value && name) {
         setTodo(
             [...todo, {
                 id: uuidv4(),
-                name: 'NewName',
+                name: name,
                 description: value,
-                date: new Date().toLocaleString(),
+                date: showDate,
                 status: true
             }]
         )
-        setValue('')
-
+        setName('');
+        setValue('');
         }
     }
 
     return (
-        <div>
-            <input placeholder="Enter exercise" value={value} onChange={(e) => setValue(e.target.value)} />
-            <button onClick={saveTodo}>Save</button>
-        </div>
+        <Row > 
+            <Col className="input-row">
+            <FormControl placeholder="Enter your name" value={name} onChange={(e) => setName(e.target.value)} />
+            <FormControl placeholder="Enter exercise" value={value} onChange={(e) => setValue(e.target.value)} />
+            <Button variant="success" onClick={ ()=> {saveTodo(); handelTime() }}>Save</Button>
+            </Col>
+            </Row>
+            
     )
 };
 
