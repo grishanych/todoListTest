e76d24f298aa54a94803c46981a1c819a1e30551
Grygorii Nych <alsolo2011@gmail.com>
2022-09-11 23:06:10 +0300

    Fixed bugs and add styles

--- src/App.js
+++ src/App.js
@@ -3,24 +3,22 @@ import './App.css';
 import AddTodo from './components/AddTodo';
 import Header from './components/Header';
 import ListTodo from './components/ListTodo';
-import {Container} from 'react-bootstrap'
 
 function App() {
 
-
   const LOCAL_STORAGE_KEY = "task-list";
-  const [todo, setTodo] = useState( JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || []);
+  const [todo, setTodo] = useState(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || []);
 
   useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todo));
   }, [todo]);
-    
+
   return (
-    <Container>
+    <div className='containerMain'>
       <Header />
       <AddTodo todo={todo} setTodo={setTodo} />
       <ListTodo todo={todo} setTodo={setTodo} />
-    </Container>
+    </div>
   );
 }
 
